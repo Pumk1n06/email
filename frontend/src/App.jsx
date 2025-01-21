e89e1bf8a54da30f2fe8ae142e01dbd35f4d6ac0
Pumk1n06 <poojithukaradi06@gmail.com>
2025-01-21 20:16:52 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import EmailEditor from "./components/EmailEditor";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1 style={{ textAlign: "center" }}>Email Template Editor</h1>
-      <EmailEditor />
+    <div style={{ padding: "20px", background: "#f4f4f4", minHeight: "100vh" }}>
+      <h1 style={{ textAlign: "center", color: "#333", marginBottom: "20px" }}>Email Template Editor</h1>
+      <EmailEditor/>
     </div>
   );
-}
+};
 
 export default App;
